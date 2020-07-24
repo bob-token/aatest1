2cd5b8671a58c15a198166f34b420e6724bbc14e
bob <xuhongbo@booyue.com.cn>
2020-07-24 10:58:45 +0800

    update

--- git-create.sh
+++ git-create.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-
 repo_name=$1
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
 
 curl -u 'xhban520@gmail.com' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 
 git init
+echo "git-create.sh" >.gitignore
 git add .
 git commit -am'first init'
 git remote add origin "https://github.com/bob-token/$repo_name.git"
