ff42a665805bc2c2eca48544682336c14cdce0dd
bob <xuhongbo@booyue.com.cn>
2020-07-24 10:35:30 +0800

    first init

---
+++ git-create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+repo_name=$1
+test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
+
+curl -u 'xhban520@gmail.com' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+
+git init
+git add .
+git commit -am'first init'
+git remote add origin "https://github.com/your_github_username/$repo_name.git"
+git push origin master
